cfd4e3129562212c8c7ccc406df474507a96e4af
Brandon Hilkert <brandonhilkert@gmail.com>
2019-06-05 08:24:12 -0400

    Allow 'show_dialog' authorization option in OmniAuth setup

--- lib/omniauth-spotify.rb
+++ lib/omniauth-spotify.rb
@@ -4,23 +4,16 @@ require 'date'
 module OmniAuth
   module Strategies
     class Spotify < OmniAuth::Strategies::OAuth2
-      # Give your strategy a name.
       option :name, 'spotify'
 
-      FORCE_APPROVAL_KEY = 'ommiauth_spotify_force_approval?'.freeze
-
+      option :authorize_options, %w[show_dialog]
       option :client_options, {
         :site          => 'https://api.spotify.com/v1',
         :authorize_url => 'https://accounts.spotify.com/authorize',
         :token_url     => 'https://accounts.spotify.com/api/token',
       }
 
-      # These are called after authentication has succeeded. If
-      # possible, you should try to set the UID without making
-      # additional calls (if the user id is returned with the token
-      # or as a URI parameter). This may not be possible with all
-      # providers.
-      uid{ raw_info['id'] }
+      uid { raw_info['id'] }
 
       info do
         {
@@ -60,9 +53,8 @@ module OmniAuth
 
       def authorize_params
         super.tap do |params|
-          if session.delete(FORCE_APPROVAL_KEY) ||
-             (session[:flash] && session[:flash]['flashes'] && session[:flash]['flashes'][FORCE_APPROVAL_KEY])
-            params[:show_dialog] = true
+          options[:authorize_options].each do |k|
+            params[k] = request.params[k.to_s] unless [nil, ''].include?(request.params[k.to_s])
           end
         end
       end
